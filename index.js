dc3ecea35710553de5e24d43dd31b6021638381c
Saswata <saswatamaitra3.14@gmail.com>
2023-06-26 21:29:02 +0530

    Here we are reapeting same function again and again  but chages the logic in side it, it is not a good way to write code, we have to write optimised code

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-// Introduction 
+// Example 2
 
-function x(){
-    console.log("X is call back function");
+const radius =[3, 4 ,5,6];
+
+
+// Area function 
+const CalculateArea = function (radius){
+    const area=[];
+    
+    for(let i=0; i<radius.length; i++){
+            area.push(Math.PI * radius[i]*radius[i]);
+    }
+    return area;
 }
 
-function Y(x)
-{
-    x();
-    console.log("Y is higher order function");
+console.log(CalculateArea(radius));
+
+
+
+// Circumfarence function 
+const CalculateCircumfarence = function (radius){
+    const Circumfarence=[];
+    
+    for(let i=0; i<radius.length; i++){
+            Circumfarence.push(2*Math.PI * radius[i]);
+    }
+    return Circumfarence;
 }
 
-Y(x);
+console.log(CalculateCircumfarence(radius));
+
+
+
+// Diameter function
+const Calculatediameter = function (radius){
+    const diameter=[];
+    
+    for(let i=0; i<radius.length; i++){
+            diameter.push(2*radius[i]);
+    }
+    return diameter;
+}
+
+console.log(Calculatediameter(radius));
+
+
